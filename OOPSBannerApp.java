0be172a13d5777d4ba15bb8eb256ecdc6c7c4dec
unknown <vinayyadaviit1805@gmail.com>
2026-03-02 09:11:28 +0530

    UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,53 +1,52 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
-                " ***** ",
-                " ***** ",
-                " ***** ",
-                " ***** "
-        ));
+        String[] lines = new String[7];
 
-        System.out.println(String.join(" ",
-                " *   * ",
-                " *   * ",
-                " *   * ",
-                " *     "
-        ));
+        lines[0] = String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** ");
 
-        System.out.println(String.join(" ",
+        lines[1] = String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *     ");
+
+        lines[2] = String.join(" ",
                 " *   * ",
                 " *   * ",
                 " ***** ",
-                " ***** "
-        ));
+                " ***** ");
 
-        System.out.println(String.join(" ",
+        lines[3] = String.join(" ",
                 " *   * ",
                 " *   * ",
                 " *     ",
-                "     * "
-        ));
+                "     * ");
 
-        System.out.println(String.join(" ",
+        lines[4] = String.join(" ",
                 " *   * ",
                 " *   * ",
                 " *     ",
-                "     * "
-        ));
+                "     * ");
 
-        System.out.println(String.join(" ",
+        lines[5] = String.join(" ",
                 " *   * ",
                 " *   * ",
                 " *     ",
-                " *   * "
-        ));
+                " *   * ");
 
-        System.out.println(String.join(" ",
+        lines[6] = String.join(" ",
                 " ***** ",
                 " ***** ",
                 " *     ",
-                " ***** "
-        ));
+                " ***** ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
