6cd711a7331ec993e22280d3d063f6abfbef7c78
unknown <vinayyadaviit1805@gmail.com>
2026-03-02 09:14:32 +0530

    UC5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,49 +1,51 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        String[] lines = new String[7];
+       
+        String[] lines = {
 
-        lines[0] = String.join(" ",
-                " ***** ",
-                " ***** ",
-                " ***** ",
-                " ***** ");
+            String.join(" ",
+                    " ***** ",
+                    " ***** ",
+                    " ***** ",
+                    " ***** "),
 
-        lines[1] = String.join(" ",
-                " *   * ",
-                " *   * ",
-                " *   * ",
-                " *     ");
+            String.join(" ",
+                    " *   * ",
+                    " *   * ",
+                    " *   * ",
+                    " *     "),
 
-        lines[2] = String.join(" ",
-                " *   * ",
-                " *   * ",
-                " ***** ",
-                " ***** ");
+            String.join(" ",
+                    " *   * ",
+                    " *   * ",
+                    " ***** ",
+                    " ***** "),
 
-        lines[3] = String.join(" ",
-                " *   * ",
-                " *   * ",
-                " *     ",
-                "     * ");
+            String.join(" ",
+                    " *   * ",
+                    " *   * ",
+                    " *     ",
+                    "     * "),
 
-        lines[4] = String.join(" ",
-                " *   * ",
-                " *   * ",
-                " *     ",
-                "     * ");
+            String.join(" ",
+                    " *   * ",
+                    " *   * ",
+                    " *     ",
+                    "     * "),
 
-        lines[5] = String.join(" ",
-                " *   * ",
-                " *   * ",
-                " *     ",
-                " *   * ");
+            String.join(" ",
+                    " *   * ",
+                    " *   * ",
+                    " *     ",
+                    " *   * "),
 
-        lines[6] = String.join(" ",
-                " ***** ",
-                " ***** ",
-                " *     ",
-                " ***** ");
+            String.join(" ",
+                    " ***** ",
+                    " ***** ",
+                    " *     ",
+                    " ***** ")
+        };
 
         for (String line : lines) {
             System.out.println(line);
