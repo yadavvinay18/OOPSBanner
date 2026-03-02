e71ec9332197b1f16d0368b1e078b36dbc6849d5
unknown <vinayyadaviit1805@gmail.com>
2026-03-02 09:08:30 +0530

    UC3

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,53 @@
 public class OOPSBannerApp {
-    public static void main(String args[]) {
-        System.out.println(" *******  " + " *******  " + " *******  " + " *******  ");
-        System.out.println("*       * " + "*       * " + "*       * " + "*       * ");
-        System.out.println("*       * " + "*       * " + "*       * " + "*         ");
-        System.out.println("*       * " + "*       * " + " *******  " + " *******  ");
-        System.out.println("*       * " + "*       * " + "*         " + "        * ");
-        System.out.println("*       * " + "*       * " + "*         " + "*       * ");
-        System.out.println(" *******  " + " *******  " + "*         " + " *******  ");
-        
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *     "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " ***** ",
+                " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                "     * "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                "     * "
+        ));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                " *   * "
+        ));
+
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                " *     ",
+                " ***** "
+        ));
     }
 }
